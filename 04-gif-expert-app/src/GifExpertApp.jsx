3fe0d78062c0762567ba33372d3d12255d6cbabb
Alejo <108655562+AlejoAcle@users.noreply.github.com>
2023-01-09 10:41:06 +0100

    Add files via upload

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([]);
+    
+    const onAddCategory = (newCategory) =>{
+
+        if(categories.includes(newCategory)) return;
+
+        // console.log(newCategory)
+        setCategories([newCategory, ...categories]);
+
+        // setCategories( [' ',...categories] )
+        
+    }
+
+
+
+
+  return (
+    <>
+        {/* Titulo */}
+        <h1>Gif Expert App</h1>
+
+        
+        <AddCategory 
+        onNewCategory={ onAddCategory }
+        />
+
+
+        
+        {/* <button onClick={onAddCategory}>Add</button> */}
+            { categories.map((category) =>
+                (
+                <GifGrid
+                    key={category}
+                    category= {category} />
+                ))
+            }
+    
+
+
+           
+
+
+
+
+
+    </>
+  )
+}
